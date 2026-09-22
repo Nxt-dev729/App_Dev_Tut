eeb8b0f953a35ce3afb0da01174bf281bd6bbe1e
Yug <yug@gmail.com>
2026-09-22 21:58:22 +0530

    commit done

--- main2209.dart
+++ main2209.dart
@@ -1,24 +1,24 @@
 /*this code past on VS Code after proper installation in main.dart file*/
 
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hello World...')),
-      ),
-    ),
-  );
-}
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Hello World...')),
+//       ),
+//     ),
+//   );
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp();
+//   }
+// }
