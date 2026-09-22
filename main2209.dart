6da42fd78ee8f45597a41cfc3e761da7d40c5d11
Your Name <you@example.com>
2026-09-22 21:22:15 +0530

    hello world in app

---
+++ main2209.dart
@@ -0,0 +1,24 @@
+/*this code past on VS Code after proper installation in main.dart file*/
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Hello World...')),
+//       ),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp();
+//   }
+// }
